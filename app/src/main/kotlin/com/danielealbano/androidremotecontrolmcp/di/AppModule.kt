c9767a8c180230488b393761cb38af9af78e1a2e
Daniele Salvatore Albano <d.albano@gmail.com>
2026-02-15 16:48:25 +0100

    feat: update manifest permissions, DI bindings, and McpToolUtils helpers
    - AndroidManifest: add QUERY_ALL_PACKAGES and KILL_BACKGROUND_PROCESSES
    - AppModule: bind StorageLocationProvider, FileOperationProvider, AppManager
    - McpToolUtils: add optionalInt and optionalBoolean parameter extractors

--- app/src/main/kotlin/com/danielealbano/androidremotecontrolmcp/di/AppModule.kt
+++ app/src/main/kotlin/com/danielealbano/androidremotecontrolmcp/di/AppModule.kt
@@ -8,10 +8,16 @@ import com.danielealbano.androidremotecontrolmcp.data.repository.SettingsReposit
 import com.danielealbano.androidremotecontrolmcp.data.repository.SettingsRepositoryImpl
 import com.danielealbano.androidremotecontrolmcp.services.accessibility.AccessibilityServiceProvider
 import com.danielealbano.androidremotecontrolmcp.services.accessibility.AccessibilityServiceProviderImpl
+import com.danielealbano.androidremotecontrolmcp.services.apps.AppManager
+import com.danielealbano.androidremotecontrolmcp.services.apps.AppManagerImpl
 import com.danielealbano.androidremotecontrolmcp.services.accessibility.ActionExecutor
 import com.danielealbano.androidremotecontrolmcp.services.accessibility.ActionExecutorImpl
 import com.danielealbano.androidremotecontrolmcp.services.screencapture.ScreenCaptureProvider
 import com.danielealbano.androidremotecontrolmcp.services.screencapture.ScreenCaptureProviderImpl
+import com.danielealbano.androidremotecontrolmcp.services.storage.FileOperationProvider
+import com.danielealbano.androidremotecontrolmcp.services.storage.FileOperationProviderImpl
+import com.danielealbano.androidremotecontrolmcp.services.storage.StorageLocationProvider
+import com.danielealbano.androidremotecontrolmcp.services.storage.StorageLocationProviderImpl
 import com.danielealbano.androidremotecontrolmcp.services.tunnel.AndroidCloudflareBinaryResolver
 import com.danielealbano.androidremotecontrolmcp.services.tunnel.CloudflaredBinaryResolver
 import dagger.Binds
@@ -83,4 +89,16 @@ abstract class ServiceModule {
 
     @Binds
     abstract fun bindCloudflareBinaryResolver(impl: AndroidCloudflareBinaryResolver): CloudflaredBinaryResolver
+
+    @Binds
+    @Singleton
+    abstract fun bindStorageLocationProvider(impl: StorageLocationProviderImpl): StorageLocationProvider
+
+    @Binds
+    @Singleton
+    abstract fun bindFileOperationProvider(impl: FileOperationProviderImpl): FileOperationProvider
+
+    @Binds
+    @Singleton
+    abstract fun bindAppManager(impl: AppManagerImpl): AppManager
 }
